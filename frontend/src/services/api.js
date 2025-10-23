4daae111a5b599817aa800b8ed49535943487ac5
Erdős Csaba <erdos2.cs@gmail.com>
2025-10-23 17:11:02 +0200

    Add axios instance with request interceptor for authentication token

---
+++ frontend/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// Create a new axios instance
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+// This function will run on *every* request before it's sent
+api.interceptors.request.use(
+  (config) => {
+    // Get the token from localStorage
+    const token = localStorage.getItem('token');
+    
+    if (token) {
+      // Add the token to the 'Authentication-Token' header
+      config.headers['Authentication-Token'] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
