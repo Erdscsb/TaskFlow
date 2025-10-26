c60f8549dd455791f4af65c3ce482aad9ec964b8
Erdős Csaba <erdos2.cs@gmail.com>
2025-10-26 17:43:02 +0100

    Initialize database within app context on startup

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,10 @@ The main entry point for the Flask application.
 
 # Import the 'app' instance from our 'app' package
 from app import app
+from app.models import app, db
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
